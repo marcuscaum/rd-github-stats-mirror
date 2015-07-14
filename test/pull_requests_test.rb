19fa136e768332dfb206656fbbe89c8003330510
Lucas André de Alencar <lucas.alencar@resultadosdigitais.com.br>
2015-07-14 16:52:51 -0300

    Fix PR test cases.

--- test/pull_requests_test.rb
+++ test/pull_requests_test.rb
@@ -2,15 +2,14 @@ require 'test_helper'
 
 describe PullRequests do
 
-  let(:prs) { PullRequests.new('rdstation', state: 'open', since: 1.day.ago) }
+  let(:prs) { PullRequests.new('rdstation', state: 'open') }
 
   it 'fetches all PRs with state specified' do
-    prs.fetch_all.wont_be_empty
+    prs.fetch.wont_be_empty
   end
 
   it 'fetches only PRs since date' do
-    time_window = 1.day.ago
-    list = prs.fetch
-    list.each { |pr| pr.updated_at.must_be :>=, time_window }
+    list = prs.fetch_since(1.day.ago)
+    list.each { |pr| pr.updated_at.must_be :>=, 1.day.ago }
   end
 end
