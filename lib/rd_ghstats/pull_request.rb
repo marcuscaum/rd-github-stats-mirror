ef9957f401562a1a3b8d50d9648a50d1c5df9cda
Lucas André de Alencar <lucas.alencar@resultadosdigitais.com.br>
2015-07-15 09:26:56 -0300

    Separate PR reports.

--- lib/rd_ghstats/pull_request.rb
+++ lib/rd_ghstats/pull_request.rb
@@ -10,14 +10,6 @@ class PullRequest
     @pull_request ||= @client.pull_request @repo_name, @number
   end
 
-  def self.count_by_state(prs)
-    ReportHelper.count(prs) { |pr| pr.state }
-  end
-
-  def self.count_by_user(prs)
-    ReportHelper.count(prs) { |pr| pr.user.login }
-  end
-
   # Comments added inside code diff
   def comments
     @comments ||= @client.pull_comments @repo_name, @number
@@ -38,13 +30,6 @@ class PullRequest
 
   def issue
     Issue.new @repo_name, @number
-  end
-
-  def self.count_all_comments(prs)
-    prs.map do |pr|
-      pr_obj = PullRequest.new(pr.head.repo.full_name, pr.number)
-      { pr.number => pr_obj.count_all_comments }
-    end
   end
 
   def commits
