4b6bdb557bdc643f5cb311a57a763e29d3c0031d
Lucas André de Alencar <alencar.lucas.a@gmail.com>
2015-07-14 22:42:56 -0300

    Add default full report.

--- lib/rd_ghstats.rb
+++ lib/rd_ghstats.rb
@@ -11,8 +11,37 @@ Octokit.configure do |c|
   c.auto_paginate = true
 end
 
+def full_report
+  puts
+  puts '=' * 80
+  repo_name = 'ResultadosDigitais/rdstation'
+  repo = Repo.new repo_name
+
+  open_prs = repo.pull_requests
+
+  puts "Number of open PRs in #{repo_name} = #{ReportHelper.count(open_prs)}"
+  puts
+
+  puts "PRs opened by user"
+  puts ReportHelper.count(open_prs) { |pr| pr.user.login }
+  puts
+
+  puts '=' * 80
+  open_issues = repo.issues
+
+  puts "#issues in #{repo_name} opened in the last week"
+  puts ReportHelper.count(open_issues)
+
+  puts "#issues by state in #{repo_name} for last week"
+  puts ReportHelper.count(repo.issues(state: 'all')) { |i| i.state }
+
+  puts "#issues opened by user in #{repo_name} in last week"
+  puts ReportHelper.count(open_issues) { |i| i.user.login }
+end
+
 repo_name = 'ResultadosDigitais/rdstation'
 repo = Repo.new repo_name
-client = Octokit.client
+
+issues = repo.issues
 
 binding.pry
