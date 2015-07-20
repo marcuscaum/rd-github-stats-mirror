ecb881607955bd50672e1ea8c44d3eeaabd56768
Lucas André de Alencar <lucas.alencar@resultadosdigitais.com.br>
2015-07-20 08:50:33 -0300

    Organize file mess.

--- lib/rd_github_stats.rb
+++ lib/rd_github_stats.rb
@@ -3,9 +3,11 @@ require 'active_support/all'
 
 require 'rd_github_stats/octokit_config'
 require 'rd_github_stats/report_helper'
-require 'rd_github_stats/repo'
-require 'rd_github_stats/pull_request'
-require 'rd_github_stats/issue'
-require 'rd_github_stats/team'
-require 'rd_github_stats/pull_requests_stats'
-require 'rd_github_stats/user_pull_requests_stats'
+
+require 'rd_github_stats/clients/repo'
+require 'rd_github_stats/clients/pull_request'
+require 'rd_github_stats/clients/issue'
+require 'rd_github_stats/clients/team'
+
+require 'rd_github_stats/stats/pull_requests_stats'
+require 'rd_github_stats/stats/user_pull_requests_stats'
